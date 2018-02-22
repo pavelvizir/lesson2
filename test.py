3a7b21c461f04523e441003101d3516bae35bf04
nobody <no@way>
2018-02-22 20:44:01 +0300

    Exclude *.pyc, remove temp test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import ephem
-from pprint import pprint
-from datetime import datetime
-pprint(ephem._libastro.builtin_planets())
-for i in list(a[2] for a in ephem._libastro.builtin_planets() if a[1] == 'Planet'):
-    t = getattr(ephem, i)
-    print(ephem.constellation(t(datetime.now().strftime('%Y/%m/%d'))))
