e03ca1e8aaf0a5fb5051b41167ad2f777140a2dc
nobody <no@way>
2018-02-22 11:57:21 +0300

    Initial commit. .gitignore creation

---
+++ bot.py
@@ -0,0 +1,35 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+logging.basicConfig(format='%(asctime)s: %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+
+def greet_user(bot, update):
+
+    text = 'Вызван /start'
+    print(text)
+    print(update)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    # update.message.reply_text(user_text[::-1].swapcase())
+    update.message.reply_text(user_text[::-1])
+
+
+def main():
+    updater = Updater("")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    updater.start_polling()
+    updater.idle()
+
+
+main()
