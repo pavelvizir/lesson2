754ba32ff95087e6d3e813dfb5c46774f742cb66
nobody <no@way>
2018-02-22 20:39:10 +0300

    Task #5 completed. Bot works, though script is dirty :-( File: bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,30 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+#!/usr/bin/env python
+'''
+Задание
+
+    1. Установите модуль ephem
+    2. Добавьте в бота команду /planet, которая будет принимать на вход
+        название планеты на английском.
+    3. При помощи условного оператора if и ephem.constellation научите бота
+        отвечать, в каком созвездии сегодня находится планета.
+
+'''
+
 import logging
-logging.basicConfig(format='%(asctime)s: %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-                    )
+from datetime import datetime
+
+import ephem
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+from bot_token import bot_token
+
+LIST_OF_PLANETS = list(
+    a[2] for a in ephem._libastro.builtin_planets() if a[1] == 'Planet')
+
+logging.basicConfig(
+    format='%(asctime)s: %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename='bot.log')
 
 
 def greet_user(bot, update):
@@ -14,6 +35,25 @@ def greet_user(bot, update):
     update.message.reply_text(text)
 
 
+def get_constellation(bot, planet, args):
+
+    text = 'Вызван /planet'
+    planet_name = ''.join(args)
+    print(text)
+    print(planet)
+
+    if planet_name in LIST_OF_PLANETS:
+        planet_object = getattr(ephem, planet_name)
+        constellation = ephem.constellation(
+                planet_object(datetime.now().strftime('%Y/%m/%d')))
+        print(constellation)
+        planet.message.reply_text(constellation)
+    else:
+        constellaton = "No such planet!"
+        print(constellaton)
+        planet.message.reply_text(constellation)
+
+
 def talk_to_me(bot, update):
     user_text = update.message.text
     print(user_text)
@@ -22,10 +62,11 @@ def talk_to_me(bot, update):
 
 
 def main():
-    updater = Updater("")
+    updater = Updater(bot_token)
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_constellation, pass_args=True))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     updater.start_polling()
@@ -33,3 +74,13 @@ def main():
 
 
 main()
+'''
+import ephem
+from pprint import pprint
+from datetime import datetime
+pprint(ephem._libastro.builtin_planets())
+
+for i in list(a[2] for a in ephem._libastro.builtin_planets() if a[1] == 'Planet'):
+    t = getattr(ephem, i)
+    print(ephem.constellation(t(datetime.now().strftime('%Y/%m/%d'))))
+    '''
